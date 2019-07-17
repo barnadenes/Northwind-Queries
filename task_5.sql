e572933930afcb30d1cf9746fdf9e60d30bb1396
barnadenes <nyukk1007@gmail.com>
2019-07-17 12:59:49 +0200

    task5 syntax error fix

--- task_5.sql
+++ task_5.sql
@@ -2,7 +2,7 @@ select company_name as Company,
 product_name as Product,
 unit_price as Price from Products
 join suppliers on suppliers.supplier_id = products.supplier_id
-join (select supplier_id, max(products.unit_price) from products
-group by products.supplier_id) as Done on products.supplier_id = Done.supplier_id
-and products.unit_price = Done.unit_price
+join (select supplier_id, max(products.unit_price) as price from products
+group by products.supplier_id) as Filtered on products.supplier_id = Filtered.supplier_id
+and products.unit_price = Filtered.price
 order by unit_price desc;
